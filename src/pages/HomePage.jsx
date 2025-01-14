4b67234f91f1c214b55ed4a49013219f8a835d9a
Siranjeevi <siranjeevi0619@gmail.com>
2025-01-14 23:20:00 +0530

    added db

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import LoginCard from "../components/LoginCard";
+
+function HomePage() {
+  return (
+    <div>
+      <Navbar />
+      <LoginCard />
+    </div>
+  );
+}
+
+export default HomePage;
